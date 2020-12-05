49d5618f6667df990869338ec5835bf6bb65f68c
Andre Winkler <andre.winkler@web.de>
2020-12-05 12:17:38 +0100

    Some tests with some documents...

--- src/test/java/de/awtools/textscanner/TextScannerTest.java
+++ src/test/java/de/awtools/textscanner/TextScannerTest.java
@@ -13,4 +13,13 @@ public class TextScannerTest {
         java.awt.image.BufferedImage image = ImageIO.read(TextScanner.class.getResourceAsStream("ocrexample.jpg"));
         System.out.println(textScanner.start(image));
     }
+
+    @Test
+    public void scanImage() throws Exception {
+        TrainedDataProvider provider = new TrainedDataProvider();
+        TextScanner textScanner = new TextScanner(provider); 
+        java.awt.image.BufferedImage image = ImageIO.read(TextScanner.class.getResourceAsStream(".rechnung-zahnarzt.jpg"));
+        System.out.println(textScanner.start(image));
+    }
+
 }
