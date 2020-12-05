35cd2c1e2e327f2e27cf7d55682df024472a1299
Andre Winkler <gluehloch@googlemail.com>
2020-12-05 15:17:26 +0100

    WIP: Upload-Controller. Redirect don´t work.

--- src/main/java/de/awtools/textscanner/Controller.java
+++ src/main/java/de/awtools/textscanner/Controller.java
@@ -1,7 +1,13 @@
 package de.awtools.textscanner;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 @RestController
 public class Controller {
@@ -10,5 +16,15 @@ public class Controller {
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
+
+	@PostMapping(value="/scan")
+	public String scan(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+		// ResponseEntity<>
+		// return ResponseEntity.ok("text");
+		redirectAttributes.addFlashAttribute("message", "You successfully uploaded " + file.getOriginalFilename() + "!");
+
+		return "redirect:/";
+	}
+	
 
 }
